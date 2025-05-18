145971b30a8d3f3aeeb228c2d082d5b8e69df1b3
anya763 <sunita1980chandra@gmail.com>
2025-05-18 17:08:55 +0530

    Add files via upload
    happy

---
+++ introduction to javascript lesson 19/types of Operators.js
@@ -0,0 +1,21 @@
+var a = 23;
+a += 32;
+console.log(a);
+
+var b = 45;
+b -= 32;
+console.log(b);
+
+var c = 3;
+c *= 32;
+console.log(c);
+
+var d = 23;
+d /= 32;
+console.log(d);
+
+var e = 23;
+e %= 32;
+console.log(e);
+
+
---
+++ introduction to javascript lesson 19/variables.js
@@ -0,0 +1,5 @@
+var Misherya = 69;
+var Aanya = 102;
+var Ishika = 123;
+var add = Misherya + Aanya + Ishika;
+console.log(add);
